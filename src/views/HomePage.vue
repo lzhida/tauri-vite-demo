453d6a3eaec31f237c86c149c34cb39469283149
林志达 <zhidal@qq.com>
2022-07-16 16:38:56 +0800

    feat: ✨ 添加默认css样式

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -3,17 +3,29 @@
  * @Author: zhidal
  * @Date: 2022-07-12 10:27:22
  * @LastEditors: zhidal
- * @LastEditTime: 2022-07-12 10:33:37
+ * @LastEditTime: 2022-07-16 16:37:54
 -->
 
 <template>
-  <div>
+  <div class="home_page">
     <img alt="Vue logo" src="@/assets/logo.png" />
     <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+    <el-button @click="handleClick">tauri api 示例</el-button>
   </div>
 </template>
 
 <script setup lang="ts">
   import HelloWorld from '@/components/HelloWorld.vue';
+  import router from '@/router';
+
+  const handleClick = () => {
+    console.log('进入示例页面');
+    router.push('example');
+  };
 </script>
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+  .home_page {
+    padding-top: 60px;
+    text-align: center;
+  }
+</style>
